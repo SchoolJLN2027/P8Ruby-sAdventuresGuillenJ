14cf94dd6ab5ba1c72ceffe003d6178a71da35ee
Julien Guillen <723506@student.dusd.net>
2023-11-07 15:25:33 -0800

    box colliders
    made some more prefabs

--- RubysAdventureGuillenJ/Assets/Scripts/RubyController.cs
+++ RubysAdventureGuillenJ/Assets/Scripts/RubyController.cs
@@ -5,21 +5,31 @@ using UnityEngine.UIElements;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    //store input data
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-       
+       rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+
+    }
+    public void FixedUpdate()
+    {
+        //uses the rigidbody position for updates
+        Vector2 position = rigidbody2d.position;
         position.y = position.y + (3.0f * vertical) * Time.deltaTime;
         position.x = position.x + (3.0f * horizontal) * Time.deltaTime;
-        transform.position = position;
-
+        //will update the rigidbody position
+        rigidbody2d.MovePosition(position);
     }
 }
