bd80dbf1436a3283229b0fee77c71dc8746e8f3e
Julien Guillen <723506@student.dusd.net>
2023-12-08 20:53:52 -0800

    made jambi complete and started sound
    12/8/23

--- RubysAdventureGuillenJ/Assets/Scripts/RubyController.cs
+++ RubysAdventureGuillenJ/Assets/Scripts/RubyController.cs
@@ -63,12 +63,16 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
             if (hit.collider != null)
             {
-                Debug.Log("raycast has hit " + hit.collider.gameObject); 
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
             }
         }
     }
